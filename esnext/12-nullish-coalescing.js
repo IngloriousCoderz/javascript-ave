7d0b0b1067ae43eef1367d26fdcca165b3765040
Matteo Antony Mistretta <m.mistretta@reply.it>
2025-09-29 12:00:52 +0200

    Create first Vue app

--- esnext/12-nullish-coalescing.js
+++ esnext/12-nullish-coalescing.js
@@ -1,4 +1,4 @@
-// falsy values
+// truthy/falsy values
 
 console.log(Boolean(1));
 console.log(!!1); // truthy
@@ -10,8 +10,16 @@ console.log(!!null);
 console.log(!!undefined);
 console.log(!!NaN);
 
+// nullish values
+
 const favoriteFood = "";
 
 console.log(favoriteFood || "cheeseburger");
 console.log(favoriteFood != null ? favoriteFood : "cheeseburger");
 console.log(favoriteFood ?? "cheesburger");
+
+const numberOfKids = 0;
+
+console.log(numberOfKids || "I don't know");
+console.log(numberOfKids != null ? numberOfKids : "I don't know");
+console.log(numberOfKids ?? "I don't know");
